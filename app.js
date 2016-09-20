772210e710c06a50290e1e181f119a1d306f4a26
NanamiTakayama <ns.potable@gmail.com>
2016-09-20 09:51:16 +0900

    0920template

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-var http = require('http');
-var options = {
-        method: 'HEAD',
-        host: '127.0.0.1/v1/ping',
-        port: '48080'
-}; // isaaxd server
-
-var req = http.request(options, function(res) {
-            console.log('Hello Isaax! %s %s', res.statusCode, res.statusMessage);
-})
-
-req.end();
+setInterval(function() {
+  console.log("Hello IoT from isaax");
+}, 5000);
